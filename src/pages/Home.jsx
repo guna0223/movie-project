08517ec16420335c70da9b76da8035d2546f380a
guna0223 <gunasekar0223@gmail.com>
2025-11-01 20:36:27 +0530

    api update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,13 @@
 import MovieCard from "../components/MovieCard/MovieCard";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovie } from "../services/api";
 
 const Home = () => {
     const [searchQuery, setSearchQuery] = useState("");
+    const [movies, searchMovies] = useState([]);
 
-    const movies = [
-        {
-            id: 1,
-            title: "John Wick",
-            release_date: "2020"
-        },
-        {
-            id: 2,
-            title: "John Wick-2",
-            release_date: "2022"
-        },
-        {
-            id: 3,
-            title: "John Wick-3",
-            release_date: "2024"
-        },
-        {
-            id: 4,
-            title: "John Wick-4",
-            release_date: "2025"
-        },
-        {
-            id: 5,
-            title: "3",
-            release_date: "2027"
-        }
-    ];
+    useEffect(() => { })
+
     const handelSearch = (e) => {
         e.preventDefault();
         alert(searchQuery);
---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "0a29d0b18a015f5b930e495750ce3de4";
+const BASE_URL = "https://www.themoviedb.org/";
+
+export const getPopularMovie = async () => {
+    const response = await fetch(`${BASE_URL}/movie/populare?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)
+        }`);
+    const data = await response.json()
+    return data.results
+};
