fbbce28b55ce2a31e1e469e1a3ed7deec5dfec2f
guna0223 <gunasekar0223@gmail.com>
2025-11-02 14:33:11 +0530

    update footer

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -8,7 +8,7 @@ function Favorites() {
     if (favorites) {
         return (
             <div className="favorites">
-                <h2>Your Favorites</h2>
+                <h2 className="headiing">Your Watchlist</h2>
                 <div className="movies-grid ">
                     {favorites.map((movie) => (
                         <MovieCard movie={movie} key={movie.id} />
