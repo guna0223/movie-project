f636056d04e5d9b16af6c9c9f3f994e260c0d777
guna0223 <gunasekar0223@gmail.com>
2026-01-31 13:07:23 +0530

    update media scren

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -52,24 +52,29 @@ const Navbar = () => {
                         </Link>
                     </li>
                 </ul>
+
+                {/* SEARCH FORM */}
+                <div className="form-section">
+                    <form onSubmit={handleSearch} className="navbar-search-form">
+                        <input
+                            type="text"
+                            placeholder="Titles, people, genres"
+                            className="navbar-search-input"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                        <button type="submit" className="navbar-search-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                            </svg>
+                        </button>
+                    </form>
+                </div>
+
             </div>
 
-            {/* SEARCH FORM */}
-            <form onSubmit={handleSearch} className="navbar-search-form">
-                <input
-                    type="text"
-                    placeholder="Titles, people, genres"
-                    className="navbar-search-input"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                />
-                <button type="submit" className="navbar-search-button">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                        <circle cx="11" cy="11" r="8"></circle>
-                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                    </svg>
-                </button>
-            </form>
+
 
             {/* TOGGLE */}
             <button
